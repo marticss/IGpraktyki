3191599b919b4d7942a6b11ada134983227527c2
marticss <40250297+marticss@users.noreply.github.com>
2018-06-26 18:23:02 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,27 +1,49 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     $(".cookie-banner").css("top", 550)
 });
 
 $(".zamknij").click(function () {
-   $(".cookie-banner").css("opacity",0)
+    $(".cookie-banner").css("opacity", 0)
 });
 
 $(document).ready(function () {
     $('.clients').slick({
         arrows: false,
-        dots: true 
+        dots: true
     });
 });
 
-$(window).scroll(function(){
+$(window).scroll(function () {
     var offset = 100;
     var duration = 800;
 
-    $(window).scroll(function(){
-        if($(this).scrollTop() > offset){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > offset) {
             $(".backto").fadeIn(duration);
-        }else{
+        } else {
             $(".backto").fadeOut(duration);
+        }
+    });
+});
+
+$(function(){
+    $(".form .contact-labels input").focusout(function(){
+        var text_val = $(this).val();
+        if(text_val = "") {
+            $(this).removeClass("stay");
+        } else{
+            $(this).addClass("stay");
+        }
+    });
+});
+
+$(function(){
+    $(".form2 .contact-labels2 input").focusout(function(){
+        var text_val = $(this).val();
+        if(text_val = "") {
+            $(this).removeClass("stay2");
+        } else{
+            $(this).addClass("stay2");
         }
     });
 });
